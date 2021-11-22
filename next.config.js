1d1c5868aeeb424d792816c7694a6d14c79a7c04
alienpingu <samuele.bertocco@gmail.com>
2021-11-22 21:36:07 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    basePath: process.env.NODE_ENV === 'production' ? `/react/molla/demo-${process.env.NEXT_PUBLIC_DEMO}` : '',
+    basePath: '',
     trailingSlash: true,
     env: {
-        PUBLIC_URL: process.env.NODE_ENV === 'production' ? `/react/molla/demo-${process.env.NEXT_PUBLIC_DEMO}/` : '/',
-        APP_URL: process.env.NODE_ENV === 'production' ? 'https://d-themes.com/react/molla/' : 'http://localhost/'
+        PUBLIC_URL: '/',
+        APP_URL: 'http://localhost/'
     }
 }
